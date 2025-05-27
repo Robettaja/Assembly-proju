c3bb1e9c38efb58be4921a547890cd9074c735d5
Robettaja <Rompula2005@gmail.com>
2025-05-27 10:26:54 +0300

    Got input manager working

--- Car simulation/Assets/Scripts/InputManager.cs
+++ Car simulation/Assets/Scripts/InputManager.cs
@@ -1,16 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputManager : MonoBehaviour
+public class InputManager : MonoBehaviour,  InputSystem_Actions.ICarActions, InputSystem_Actions.ICameraActions
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputSystem_Actions inputAction; 
+    
+    private Vector2 carInput = Vector2.zero;
+    private Vector2 cameraInput= Vector2.zero;
+    public static InputManager Instance;
+
+
+    private void Start()
     {
-        
+        Instance = this;
+
+        inputAction = new InputSystem_Actions();
+        inputAction.Car.SetCallbacks(this);
+        inputAction.Camera.SetCallbacks(this);
+
+        inputAction.Car.Enable();
+        inputAction.Camera.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        carInput = context.ReadValue<Vector2>();
     }
+    public void OnCamMove(InputAction.CallbackContext context)
+    {
+        cameraInput = context.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetNormalizedCarInput()
+    {
+        return carInput.normalized;
+    }
+
+    public Vector2 GetNormalizedCameraInput()
+    {
+       return cameraInput.normalized; 
+    }
+
 }
