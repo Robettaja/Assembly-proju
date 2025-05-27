ea1deb299b4e9c4e4cda5f1626269ebdbc4ae193
Saija <saija21006@student.hamk.fi>
2025-05-27 14:32:04 +0300

    videon kanssa opettelua. tallennus tähän väliin

--- Website/client/app/src/App.jsx
+++ Website/client/app/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { use, useEffect,useState } from 'react'
 import './App.css'
 
 function App() {
   const [usernames, setUsernames] = useState([]);
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [newTitle, setNewTitle] = useState("");
 
   useEffect(() => {
     fetchUsernames();
@@ -12,22 +16,97 @@ function App() {
     try {
       const response = await fetch("http://127.0.0.1:8000/api/usernames/");
       const data = await response.json();
-      console.log(data);
+      setUsernames(data);
     } catch (err) {
       console.log(err);
     }
   };
 
+  const addUser = async () => {
+    const userData = {
+      user: user,
+      email: email,
+    };
+
+    try {
+    
+      const response = await fetch("http://127.0.0.1:8000/api/usernames/create/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+
+      });
+      const data = await response.json();
+      setUsernames((prev) => [...prev, data]);
+      console.log(data);
+    }
+     catch (err) {
+        console.log(err);
+      }
+    
+  };
+
+  const updateTitle = async (pk, email) => {
+     const userData = {
+      user: newTitle,
+      email: email,
+    };
+
+    try {
+    
+      const response = await fetch('http://127.0.0.1:8000/api/usernames/${pk}', {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+
+      });
+      const data = await response.json();
+      setUsernames((prev) => 
+        prev.map((user) => {
+          if (book.id === pk)
+          {
+            return data;
+          }
+          else {
+            return user;
+          }}
+         
+        )
+      );
+      console.log(data);
+    }
+     catch (err) {
+        console.log(err);
+      }
+  } 
+
+
   return (
     <>
-      <h1> Book website </h1>
+      <h1> Auto kilpailu </h1>
 
       <div>
-        <input type="text" placeholder="Input your username"/>
-        <input type="text" placeholder="Input your email (optional)"/>
-        <button> Add book </button>
+        <input type="text" placeholder="Input your username" 
+        onChange = {(e) => setUser(e.target.value)}
+        />
+        <input type="text" placeholder="Input your email (optional)"
+        onChange = {(e) => setEmail(e.target.value)}
+        />
+        <button onClick = {addUser}> Submit </button>
       </div>
-    </>
+      {usernames.map((username) => 
+      <div> 
+        <p>Username: {username.user} </p> 
+        <p>Email: {username.email} </p>  
+        <input type ="text" placeholder="New title..." />
+        <button onClick={ () => updateTitle(user.id, user.email)}>Change</button>
+        </div>
+      )}
+          </>
   )
 }
 
