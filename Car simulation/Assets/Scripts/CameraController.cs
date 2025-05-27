c3d7dd62f8c6a272785c1052ed01f783b8aca463
R0b4ttaja <Rompula2005@gmail.com>
2025-05-27 09:12:23 +0300

    Added some files

---
+++ Car simulation/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
