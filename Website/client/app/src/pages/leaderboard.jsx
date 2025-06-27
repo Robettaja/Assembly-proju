e6b4e6dbd6c7a7c92506bf688d2829999e696fb2
soijj98 <saijajoronen@gmail.com>
2025-06-27 14:37:09 +0300

    leaderboard styling

--- Website/client/app/src/pages/leaderboard.jsx
+++ Website/client/app/src/pages/leaderboard.jsx
@@ -9,29 +9,34 @@ const Leaderboard = ({usernames}) => {
     const topUser = sorted[0]
 
     return (
-        <div className="leaderboard">
-            <h1>Leaderboard</h1>
-            <table className="leaderboard-table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Username</th>
-                        <th>Total time</th>
-                        <th>Lap 1</th>
-                        <th>Lap 2</th>
-                        <th>Lap 3</th>
+        <div className="App">
+        <div className="p-4">
+            <h1 class="m-6 text-white text 2x1">Leaderboard</h1>
+            <div className="overflow-auto max-h-[700vh] border rounded sh">    
+            <table class="min-w-full ">
+                <thead class="bg-gray-200 sticky top-0 z-10">
+                    <tr class="shadow-md my-8">
+                        <th class="p-2">#</th>
+                        <th class="p-2">Username</th>
+                        <th class="p-2">Total time</th>
+                        <th class="p-2">Lap 1</th>
+                        <th class="p-2">Lap 2</th>
+                        <th class="p-2">Lap 3</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td colSpan={6} className="h-4"></td>
+                    </tr>
                     {sorted.length > 0 ? (
                         sorted.slice(0,10).map((user, index) => (
-                        <tr key={user.id}>
-                            <td>{index + 1}</td>
-                            <td>{user.user}</td>
-                            <td>{user.laptime}</td>
-                            <td>{user.lap1 || '-'}</td>
-                            <td>{user.lap2 || '-'}</td>
-                            <td>{user.lap3 || '-'}</td>
+                        <tr key={user.id} class="border-b bg-gray-100 ">
+                            <td class="border-r p-4">{index + 1}</td>
+                            <td class="border-r">{user.user}</td>
+                            <td class="border-r p-2">{user.laptime}</td>
+                            <td class="border-r p-2">{user.lap1 || '-'}</td>
+                            <td class="border-r p-2">{user.lap2 || '-'}</td>
+                            <td class=" p-2">{user.lap3 || '-'}</td>
                         </tr>
                         ))
                     ) : (
@@ -41,8 +46,9 @@ const Leaderboard = ({usernames}) => {
                     )}
                     </tbody>
             </table>
-
+            </div>
         </div>
+    </div>
     );
 };
 
